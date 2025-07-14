5c0a8b52cb074ce2d055cdf85641a23fba1faf3b
zzinue <zzinue@gmail.com>
2025-07-14 10:58:26 -0600

    ::

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,13 @@ import {useFonts} from "expo-font";
 
 export default function RootLayout() {
     const [loaded] = useFonts({
-        "Jakarta-Bold": require("../../uber-clone2/assets/fonts/PlusJakartaSans-Bold.ttf"),
-        "Jakarta-ExtraBold": require("../../uber-clone2/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-        "Jakarta-ExtraLight": require("../../uber-clone2/assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-        "Jakarta-Light": require("../../uber-clone2/assets/fonts/PlusJakartaSans-Light.ttf"),
-        "Jakarta-Medium": require("../../uber-clone2/assets/fonts/PlusJakartaSans-Medium.ttf"),
-        "Jakarta-Regular": require("../../uber-clone2/assets/fonts/PlusJakartaSans-Regular.ttf"),
-        "Jakarta-SemiBold": require("../../uber-clone2/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+        "Jakarta-Bold": require("../../uber/assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "Jakarta-ExtraBold": require("../../uber/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "Jakarta-ExtraLight": require("../../uber/assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+        "Jakarta-Light": require("../../uber/assets/fonts/PlusJakartaSans-Light.ttf"),
+        "Jakarta-Medium": require("../../uber/assets/fonts/PlusJakartaSans-Medium.ttf"),
+        "Jakarta-Regular": require("../../uber/assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "Jakarta-SemiBold": require("../../uber/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
     });
 
     // if (!loaded) {
